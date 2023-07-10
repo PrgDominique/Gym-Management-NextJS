c868fc8ae4971be6ad292878eb609f595a36b596
PrgDominique <dominiquesimbillo10@gmail.com>
2023-07-10 18:39:27 +0800

    added sample form for signup

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,15 @@
-export default function Register () {
-    return <h1>Register</h1>
+export default function Register() {
+  return (
+    <div className='h-100 my-40'>
+      <div className='flex justify-center '>
+        <h1 className=''>Register</h1>
+        <div className='grid grid-cols-2 w-60 h-28 my-16'>
+          <div>Username: </div>
+          <input className='border-2 rounded-md border-slate-800 h-8 w-48' />
+          <div>Password: </div>
+          <input className='border-2 rounded-md border-slate-800 h-8 w-48' type='password'/>
+        </div>
+      </div>
+    </div>
+  )
 }
--- app/sampleQuery/page.tsx
+++ app/sampleQuery/page.tsx
@@ -2,13 +2,13 @@
 
 import { gql } from '@apollo/client'
 import { useQuery } from '@apollo/experimental-nextjs-app-support/ssr'
-import { useEffect } from 'react'
 
 const GET_DOGS = gql`
-  query GetDogs {
-    dogs {
+  query {
+    username {
       id
-      breed
+      name
+      age
     }
   }
 `
@@ -20,22 +20,18 @@ interface DogsProps {
 export default function Dogs({ onDogSelected }: DogsProps) {
   const { loading, error, data } = useQuery(GET_DOGS)
 
-  useEffect(() => {
-    if (data) {
-      // Do something with the fetched data if needed
-    }
-  }, [data])
 
   if (loading) return 'Loading...'
   if (error) return `Error! ${error.message}`
 
   return (
-    <select name='dog' onChange={(e) => onDogSelected(e.target.value)}>
-      {data.dogs.map((dog: { id: string; breed: string }) => (
-        <option key={dog.id} value={dog.breed}>
-          {dog.breed}
-        </option>
-      ))}
-    </select>
+    <h1>
+    {data.username.map((item, i) => (
+      <div key={i}>
+        <h1>name: {item.name}</h1>
+        <h1>age: {item.age}</h1>
+      </div>
+    ))}
+  </h1>
   )
 }
