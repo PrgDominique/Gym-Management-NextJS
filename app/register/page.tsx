f7bff1a6def1cde8abfa81869048dd2370b545e1
PrgDominique <dominiquesimbillo10@gmail.com>
2023-07-15 11:23:13 +0800

    implement the reusable component

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -2,6 +2,8 @@
 
 import { gql, useMutation } from '@apollo/client'
 import { ChangeEvent, FormEvent, useState } from 'react'
+import { TextField } from '../components/ui/textfield'
+import { Button } from '../components/ui/button'
 
 interface User {
   id: string
@@ -22,14 +24,13 @@ export default function Register() {
   const signUp = gql`
     mutation Mutation($name: String!, $password: String!) {
       Signup(name: $name, password: $password) {
-        id
         name
         password
       }
     }
   `
 
-  const [SignupMutation, { data, loading, error }] = useMutation<
+  const [SignupMutation, { loading, error }] = useMutation<
     SignupData,
     SignupVariables
   >(signUp)
@@ -38,10 +39,6 @@ export default function Register() {
 
   if (error) return `Submission error ${error.message}`
   if (loading) return <h1>Loading ...</h1>
-
-  // console.log(data)
-  // console.log(loading)
-  // console.log(error)
 
   const handleSignUp = (e: FormEvent) => {
     e.preventDefault() // Prevent the default form submission
@@ -53,38 +50,24 @@ export default function Register() {
     })
   }
 
-  const handleUsernameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value)
-  }
-
-  const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value)
-  }
   return (
     <div className='h-100 my-40'>
       <div className='flex justify-center '>
         <h1 className=''>Register</h1>
         <div className='grid grid-cols-2 w-60 h-28 my-16'>
           <form onSubmit={handleSignUp}>
-            <div>Username: </div>
-            <input
-              className='border-2 rounded-md border-slate-800 h-8 w-48'
-              value={userName}
-              onChange={handleUsernameChange}
+            <TextField
+              name='Username'
+              onChange={(e) => setUsername(e.target.value)}
             />
-            <div>Password: </div>
-            <input
-              className='border-2 rounded-md border-slate-800 h-8 w-48'
+
+            <TextField
+              name='Password'
+              onChange={(e) => setPassword(e.target.value)}
               type='password'
-              value={password}
-              onChange={handlePasswordChange}
             />
-            <button
-              className='rounded-l-lg bg-sky-400 grid-cols-2'
-              onClick={handleSignUp}
-            >
-              Submit
-            </button>
+
+            <Button name='Sign up' type='submit' />
           </form>
         </div>
       </div>
