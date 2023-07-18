7daa87320824d3e477e3a0dd61beb5aa1cfd2fc3
PrgDominique <dominiquesimbillo10@gmail.com>
2023-07-18 16:20:49 +0800

    added value for future reusable purposes in textfield

--- app/components/ui/textfield.tsx
+++ app/components/ui/textfield.tsx
@@ -3,10 +3,11 @@ import { ChangeEvent } from 'react';
 type PassProps = {
     name: string;
     type: string;
+    value: (e: ChangeEvent<HTMLInputElement>) => void
     onChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
-export const TextField = ({name, onChange, type}: Partial<PassProps> )=> {
+export const TextField = ({name, onChange, type, value}: Partial<PassProps> )=> {
   return (
     <div>
       <div>{name} </div>
@@ -14,6 +15,7 @@ export const TextField = ({name, onChange, type}: Partial<PassProps> )=> {
         className='border-2 rounded-md border-slate-800 h-8 w-48'
         type={type}
         onChange={onChange}
+        value={value}
       />
     </div>
   )
