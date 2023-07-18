0e9223e386b1c31631c6e1879b058601259705d5
PrgDominique <dominiquesimbillo10@gmail.com>
2023-07-18 16:23:32 +0800

    added verify for email and hash for password

--- backend/GraphQL/src/mutation/createUser.ts
+++ backend/GraphQL/src/mutation/createUser.ts
@@ -1,11 +1,31 @@
 import { Users } from '../model.js'
 
-export const createUser = async (name: String, password: String) => {
+import bcrypt from 'bcrypt'
+
+export const createUser = async (
+  firstname: String,
+  lastname: String,
+  email: String,
+  password: String
+) => {
   try {
-    const username = await new Users({ name, password })
+    const hashPassword = await bcrypt.hash(password, 10)
+
+    const userExist = await Users.findOne({ email })
+
+    if (userExist) throw new Error('Email already exist')
+
+    const username = await new Users({
+      firstname,
+      lastname,
+      email,
+      password: hashPassword,
+    })
+
     await username.save()
     return username
   } catch (error) {
+    console.error('Error creating user:', error.message)
     throw new Error('Failed to create user.')
   }
 }
